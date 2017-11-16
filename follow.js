4921ab9e0ffa2a42dd83d07c27f3e0a4173205bc
suleimy <33736942+suleimy@users.noreply.github.com>
2017-11-16 15:28:53 -0800

    Create follow.js

---
+++ follow.js
@@ -0,0 +1,67 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+let ball = { x: 250, y: 150, radius: 25, color: "lemonchiffon" };
+let enemy = { x: 250, y: 250, width: 30, color: "gold" };
+let mouse = { x: 0, y: 0 };
+
+function updateMouse(event) {
+  const { left, top } = canvas.getBoundingClientRect();
+  mouse.x = event.clientX - left;
+  mouse.y = event.clientY - top;
+}
+
+document.body.addEventListener("mousemove", updateMouse);
+
+function clearBackground() {
+  ctx.fillStyle = "lightgreen";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawBall() {
+  ctx.fillStyle = ball.color;
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.stroke();
+}
+
+function drawEnemy() {
+  ctx.fillStyle = enemy.color;
+  ctx.fillRect(
+    enemy.x - enemy.width / 2,
+    enemy.y - enemy.width / 2,
+    enemy.width,
+    enemy.width
+  );
+  ctx.strokeRect(
+    enemy.x - enemy.width / 2,
+    enemy.y - enemy.width / 2,
+    enemy.width,
+    enemy.width
+  );
+}
+
+function moveToward(leader, follower, speed) {
+  follower.x += (leader.x - follower.x) * speed;
+  follower.y += (leader.y - follower.y) * speed;
+}
+
+function updateScene() {
+  ball.x += (mouse.x - ball.x) * 0.05;
+  ball.y += (mouse.y - ball.y) * 0.05;
+  
+  enemy.x += (ball.x - enemy.x) * 0.02;
+  enemy.y += (ball.y - enemy.y) * 0.02;
+}
+
+function drawScene() {
+  clearBackground();
+  drawBall();
+  drawEnemy();
+  updateScene();
+  requestAnimationFrame(drawScene);
+}
+
+requestAnimationFrame(drawScene);
+
